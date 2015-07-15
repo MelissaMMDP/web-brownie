7fee9de0798c87c64f6fa6181ed59c9314c65c69
Melissa Austin <MAustinMMDP@users.noreply.github.com>
2015-07-15 14:10:43 -0700

    Update shell.controller.js
    remove unused dependency injection code

--- src/client/app/layout/shell.controller.js
+++ src/client/app/layout/shell.controller.js
@@ -5,22 +5,12 @@
         .module('app.layout')
         .controller('Shell', Shell);
 
-    //Shell.$inject = [''];
-
     /* @ngInject */
     function Shell() {
         /* jshint validthis: true */
         var vm = this;
 
-        vm.activate = activate;
         vm.mainHeading = 'Web Brownie';
-
-        activate();
-
-        ////////////////
-
-        function activate() {
-        }
     }
 
 }());

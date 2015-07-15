ad0fb8c9a9de03bfd155c317b0b94b7decc47e89
Melissa Austin <MAustinMMDP@users.noreply.github.com>
2015-07-15 14:10:01 -0700

    Update color.controller.js
    remove unused dependency injection code

--- src/client/app/color/color.controller.js
+++ src/client/app/color/color.controller.js
@@ -5,22 +5,12 @@
         .module('app')
         .controller('Color', Color);
 
-    //Color.$inject = [''];
-
     /* @ngInject */
     function Color() {
         /* jshint validthis: true */
         var vm = this;
 
-        vm.activate = activate;
         vm.mainHeading = 'Color Scheme Framework';
-
-        activate();
-
-        ////////////////
-
-        function activate() {
-        }
     }
 
 }());

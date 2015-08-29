06c127cde7365597709427ee256296672655ed60
Melissa Austin <maustin.mmdp@outlook.com>
2015-08-28 17:48:58 -0700

    General reformat

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-var gulp = require('gulp');
 var args = require('yargs').argv;
 var browserSync = require('browser-sync');
 var config = require('./gulp.config')();
 var del = require('del');
+var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({lazy: true});
+
 var port = process.env.PORT || config.defaultPort;
 
 /**
